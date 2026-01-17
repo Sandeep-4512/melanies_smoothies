298021fbe3742611b09c4ef99f55ceadaf32b675
Sandeep Kumar Peddareddy <sandeep4512reddy@gmail.com>
2026-01-17 17:39:19 +0530

    Update dataframe handling in streamlit_app.py
    Converted Snowpark DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -10,9 +11,6 @@ st.write(
   """
 )
 
-
-
-
 order_name=st.text_input('Name on the Smoothie:')
 if order_name:
     st.write('The name on your Smoothie will be ',order_name)
@@ -20,9 +18,14 @@ if order_name:
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()                                                                                       
-                                                                                            
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()                                                                                       
+
+# Convert the Snowpark Dataframe to Pandas Dataframe
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list=st.multiselect(
     'Choose upto 5 ingedients:',
     my_dataframe,
